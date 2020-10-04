1ccb789eeccbe385b627c3b76bc656f97eb35064
Ananya Kedia <69528295+ananyak09@users.noreply.github.com>
2020-10-04 19:06:47 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,23 @@
+class Player{
+    constructor(){
+
+    }
+    getCount(){
+        var getCountref = database.ref('playerCount');
+        getCountref.on("value",function(data){
+            playerCount = data.val()
+        })
+    }
+    updateCount(count){
+        database.ref('/').update({
+            playerCount : count
+        })
+    }
+    update(name){
+        var playerIndex = "player"+playerCount;
+        database.ref(playerIndex).set({
+            name : name
+        })
+    }
+
+}
